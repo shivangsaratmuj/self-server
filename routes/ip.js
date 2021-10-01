a866a5afe67abf68d1c54d5ef67b016d33f4baad
shivang-sarat <shivang.sarat@gaadi.com>
2021-10-01 10:08:55 +0530

    Hacktoberfest 2021: Sent ip in json response for route IP

--- routes/ip.js
+++ routes/ip.js
@@ -4,7 +4,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let local_ip = req.connection.remoteAddress.split(":")[3];
-  res.status(200).json({ hey:"Hey" });
+  res.status(200).json({ hey:"Hey", ip: local_ip });
 });
 
 module.exports = router;
